77062e19492132487ef96908d11fcb63e4c1c2b0
fgaravaglia <francesco.garavaglia@gmail.com>
2019-04-16 16:23:30 +0200

    added version for nuget

--- src/Umbrella.Xlsx/Properties/AssemblyInfo.cs
+++ src/Umbrella.Xlsx/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Umbrella.Xlsx")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library to manage XLSX files")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Innova Solution")]
 [assembly: AssemblyProduct("Umbrella.Xlsx")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
